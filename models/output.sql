a837c6eac70074a66a45eb00a5b9708b9be8a8c5
Martin Matejka <12933713+Matejkys@users.noreply.github.com>
2025-03-04 11:25:25 +0100

    Update output.sql

--- models/output.sql
+++ models/output.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 
--- This model creates the "output" table by selecting all rows from the "recipients" table.
+-- Create the "output" table by selecting all rows from the external "recipients" table.
 SELECT *
-FROM {{ ref('recipients') }}
+FROM {{ source('my_source', 'recipients') }}
